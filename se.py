4437bc33379a67423604abf91cf018763117d8da
themisch <*>
2023-05-23 20:26:19 +0200

    Cosmetic changes.

--- se.py
+++ se.py
@@ -5,7 +5,7 @@ from enum import Enum, auto
 class Config:
     newlines = ("\n", "\r\n")
     default_newline = newlines[0]
-    tabsize = 4
+    tab_size = 4
 
 
 class Selection:
@@ -14,12 +14,16 @@ class Selection:
         self.end = end
         self.chars_into_line = offset
 
+
 class LineDir(Enum):
-    Up =    auto()
-    Down =  auto()
-    Left =  auto()
+    Up = auto()
+    Down = auto()
+    Left = auto()
     Right = auto()
 
+
+# This class allows modification of selections and the text itself, according 
+# to any given selections.
 class Text:
     def __init__(self, path: str = None):
         self.newline = Config.default_newline
@@ -38,7 +42,8 @@ class Text:
     def write(self):
         with open(self.path, "w") as f:
             f.write(self.data)
-
+    
+    # Return text in `select`
     def selected_text(self, select: Selection) -> str:
         return self.data[select.start : select.end]
 
@@ -47,13 +52,15 @@ class Text:
         # Count the number of newline characters before the start of the selection.
         return self.data.count(self.newline, 0, select.start)
 
-    # Move the selection to a specific line and/or char in the data.
-    def line(self, select: Selection, line_num: int, x_pos: int= 1, cache: [str] = None) -> Selection:
+    # Move the `select` to a specific line and/or char in the data.
+    def line(self, select: Selection, line_num: int, x_pos: int = 1, cache: [str] = None) -> Selection:
+
+        # temporarily create a list of lines of text, or use the cache
         lines = cache
         if not cache:
             lines = self.data.split(self.newline)[:-1]
 
-        # use the correct line
+        # use the correct line (wrap lines around max)
         line_num %= len(lines)
 
         # if specified, set the x starting pos
@@ -72,7 +79,7 @@ class Text:
             cur_len = len(lines[line_num])
             select.chars_into_line = chars_into_line - cur_len
 
-            return self.line(select, (line_num + 1) % len(lines), cache = lines)
+            return self.line(select, (line_num + 1) % len(lines), cache=lines)
 
         # find the starting index of `line_num`
         start = chars_into_line - 1  # -1 since start is an index, and chars_into_line is not
@@ -89,7 +96,7 @@ class Text:
 
         return Selection(start, end, select.chars_into_line)
 
-    # Move around using deltas.
+    # Move around the given selection using deltas instead of absolute values.
     def line_delta(self, select: Selection, direction: LineDir, delta: int = 1) -> Selection:
         delta = abs(delta)
         if direction in LineDir:
@@ -115,9 +122,9 @@ class Text:
         self.data = self.data[: select.start] + self.data[select.end :]
 
     # Wrapper around insert & delete
-    def replace(self, select: Selection, what: str) -> None:
+    def replace(self, select: Selection, With: str) -> None:
         self.delete(select)
-        self.insert(select, what)
+        self.insert(select, With)
 
 
 sel = Selection(0, 2, 1)
@@ -136,9 +143,9 @@ YOh
 print(f"'{t.selected_text(sel)}'")
 print(f"cur line: {t.line_num(sel)}")
 
-sel = t.line_delta(sel, LineDir.Down, 2)
+sel = t.line_delta(sel, LineDir.Down, delta=2)
 sel = t.line(sel, line_num=0, x_pos=4)
-#sel = t.line_delta(sel, LineDir.Right, 3)
+# sel = t.line_delta(sel, LineDir.Right, 3)
 
 print(f"cur line: {t.line_num(sel)}")
 
